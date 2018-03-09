5f85dbfa0a8ebf00173c56e9b3a489dc01bcee31
Nikki Tan <nikkitan@mercari.com>
2018-03-09 13:45:41 -0800

    Implemented welcome webhook.

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -18,6 +18,8 @@ func NewLiveChatController() controller.Controller {
 	return &LiveChatController{
 		Routes: []string{
 			"get;/;Home",
+			"get;/botengine/prohibiteditems;ProhibitedItems",
+			"get;/botengine/welcome;Welcome",
 		},
 	}
 }
@@ -26,5 +28,34 @@ func NewLiveChatController() controller.Controller {
 func (c *LiveChatController) Home() {
 	fmt.Println("Home!")
 	c.Ctx.Template = "index"
+	c.HTML(http.StatusOK)
+}
+
+// ProhibitedItems is webhook for when the user asks about prohibited items.
+func (c *LiveChatController) ProhibitedItems() {
+	fmt.Println("Welcome!")
+	c.Ctx.Template = "index"
+	c.HTML(http.StatusOK)
+}
+
+// Welcome is responsible for getting user info when the Bot first welcomed the user.
+func (c *LiveChatController) Welcome() {
+	fmt.Println("Welcome!")
+	req := c.Ctx.Request()
+	err := req.ParseForm()
+	if err != nil {
+		fmt.Println("[ERR]: " + err.Error())
+		c.HTML(http.StatusBadRequest)
+		c.Ctx.Template = "error"
+		return
+	}
+	t := req.Form.Get("token")
+	if t != "nikkitesttoken" {
+		fmt.Println("[ERR]: " + err.Error())
+		c.HTML(http.StatusUnauthorized)
+		c.Ctx.Template = "error"
+		return
+	}
+	c.Ctx.Write([]byte("challenge"))
 	c.HTML(http.StatusOK)
 }
