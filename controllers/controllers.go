07e05413cf11acb2f4d7de815a852732661647b2
Nikki Tan <nikkitan@mercari.com>
2018-03-09 15:18:29 -0800

    Added Welcome() handler for POST.

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -19,7 +19,8 @@ func NewLiveChatController() controller.Controller {
 		Routes: []string{
 			"get;/;Home",
 			"get;/botengine/prohibiteditems;ProhibitedItems",
-			"get;/botengine/welcome;Welcome",
+			"get;/botengine/welcome;VerifyWelcome",
+			"post;/botengine/welcome;Welcome",
 		},
 	}
 }
@@ -38,6 +39,28 @@ func (c *LiveChatController) ProhibitedItems() {
 	c.HTML(http.StatusOK)
 }
 
+// VerifyWelcome is called when we first verify the webhook for Welcome interaction.
+func (c *LiveChatController) VerifyWelcome() {
+	fmt.Println("VerifyWelcome!")
+	req := c.Ctx.Request()
+	err := req.ParseForm()
+	if err != nil {
+		fmt.Println("[ERR]: " + err.Error())
+		c.HTML(http.StatusBadRequest)
+		c.Ctx.Template = "error"
+		return
+	}
+	t := req.Form.Get("token")
+	if len(t) > 0 && t != "nikkitesttoken" {
+		fmt.Println("[ERR]: " + err.Error())
+		c.HTML(http.StatusUnauthorized)
+		c.Ctx.Template = "error"
+		return
+	}
+	c.Ctx.Write([]byte(req.Form.Get("challenge")))
+	c.HTML(http.StatusOK)
+}
+
 // Welcome is responsible for getting user info when the Bot first welcomed the user.
 func (c *LiveChatController) Welcome() {
 	fmt.Println("Welcome!")
@@ -49,13 +72,6 @@ func (c *LiveChatController) Welcome() {
 		c.Ctx.Template = "error"
 		return
 	}
-	t := req.Form.Get("token")
-	if t != "nikkitesttoken" {
-		fmt.Println("[ERR]: " + err.Error())
-		c.HTML(http.StatusUnauthorized)
-		c.Ctx.Template = "error"
-		return
-	}
-	c.Ctx.Write([]byte(req.Form.Get("challenge")))
+	fmt.Printf("[FORM]: %+v\n", req.Form)
 	c.HTML(http.StatusOK)
 }
